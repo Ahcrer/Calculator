9def4d06d93dac1084ba6ad0a2a9828d0b295371
Brandon Cha <99994002+Ahcrer@users.noreply.github.com>
2023-02-24 12:38:07 -0700

    credenciales
    Se modificó el código para agregar credenciales.

--- main.py
+++ main.py
@@ -7,6 +7,32 @@
 import funciones
 
 def main():
+    print("")
+    usuario = ""
+    contrasena = ""
+    continuar0 = "a"
+    while continuar0 == "a":
+        print("Bienvenido a calculadora para GCS\n¿Qué desea hacer?")
+        print("1-Ingresar\n2-Registrarse\n0-Salir")
+        operacion0 = input()
+        if operacion0 == "1":
+            usuario = input("Ingresar usuario: ")
+            contrasena = input("Ingresar contraseña: ")
+            validar_credenciales(usuario, contrasena)
+            while True:
+                operaciones()
+                return False
+        elif operacion0 == "2":
+            usuario = input("Crear usuario: ")
+            contrasena = input("Crear contraseña: ")
+            crear_usuario(usuario, contrasena)
+        elif operacion0 == "0":
+            #Se sale del ciclo y cierra el programa
+            continuar0 = "b"
+        else:
+            print("Opcion invalida")
+
+def operaciones():
     continuar = True
     while continuar:
         print('Seleccione una operación:')
@@ -29,6 +55,18 @@ def main():
             continuar = False
         else:
             print('Entrada incorrecta')
+
+def crear_usuario(usuario, contrasena):
+    with open("password.txt", "a") as archivo:
+        archivo.write(f"{usuario},{contrasena}\n")
+
+def validar_credenciales(usuario, contrasena):
+    with open("password.txt", "r", encoding="utf-8") as archivo:
+        for linea in archivo:
+            usuario_archivo, contrasena_archivo = linea.strip().split(",")
+            if usuario == usuario_archivo and contrasena == contrasena_archivo:
+                return True
+        return False
 
 if __name__ == "__main__":
     main()
