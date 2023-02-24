bc98b7be38b38ec3d751946c889735951ff9baba
Brandon Cha <99994002+Ahcrer@users.noreply.github.com>
2023-02-24 10:32:32 -0700

    creación funciones
    Se movieron las funciones para tener el main más limpio.

---
+++ funciones.py
@@ -0,0 +1,21 @@
+
+
+def sumar():
+    num1 = input("Ingresar el primer número: ")
+    num2 = input("ingresar el segundo número: ")
+    print("La suma es: " + str(int(num1) + int(num2)))
+
+def restar():
+    num1 = input("Ingresar el primer número: ")
+    num2 = input("ingresar el segundo número: ")
+    print("La resta es: " + str(int(num1) - int(num2)))
+
+def multiplicar():
+    num1 = input("Ingresar el primer número: ")
+    num2 = input("ingresar el segundo número: ")
+    print("La multiplicación es: " + str(int(num1) * int(num2)))
+
+def dividir():
+    num1 = input("Ingresar el primer número: ")
+    num2 = input("ingresar el segundo número: ")
+    print("La división es: " + str(int(num1) / int(num2)))
--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 #3.Multiplicar
 #4.Dividir
 
+import funciones
+
 def main():
     continuar = True
     while continuar:
@@ -12,41 +14,21 @@ def main():
         operacion = input()
         if operacion == "1":
             #codigo para sumar
-            sumar()
+            funciones.sumar()
         elif operacion == "2":
             #codigo para restar
-            restar()
+            funciones.restar()
         elif operacion == "3":
             #codigo para multiplicar
-            multiplicar()
+            funciones.multiplicar()
         elif operacion== "4":
             #codigo para dividir
-            dividir()
+            funciones.dividir()
         elif operacion== "0":
             #Finalizar
             continuar = False
         else:
             print('Entrada incorrecta')
-
-def sumar():
-    num1 = input("Ingresar el primer número: ")
-    num2 = input("ingresar el segundo número: ")
-    print("La suma es: " + str(int(num1) + int(num2)))
-
-def restar():
-    num1 = input("Ingresar el primer número: ")
-    num2 = input("ingresar el segundo número: ")
-    print("La resta es: " + str(int(num1) - int(num2)))
-
-def multiplicar():
-    num1 = input("Ingresar el primer número: ")
-    num2 = input("ingresar el segundo número: ")
-    print("La multiplicación es: " + str(int(num1) * int(num2)))
-
-def dividir():
-    num1 = input("Ingresar el primer número: ")
-    num2 = input("ingresar el segundo número: ")
-    print("La división es: " + str(int(num1) / int(num2)))
 
 if __name__ == "__main__":
     main()
