f0be43cdadb87170a380220ae0e88e7b2d039b23
Brandon Cha <99994002+Ahcrer@users.noreply.github.com>
2023-02-27 22:35:01 -0700

    mejora
    Se cambió el archivopassword.txt por ususarios.csv y se agregó un try para que la división entre 0 no de error

--- main.py
+++ main.py
@@ -3,8 +3,11 @@
 #2.Restar
 #3.Multiplicar
 #4.Dividir
+#Tendrá un sistema de usuario y contraseña para que solo los miembros registrados
+#puedan utilizarla.
 
 import funciones
+import csv
 
 def main():
     print("")
@@ -18,14 +21,14 @@ def main():
         if operacion0 == "1":
             usuario = input("Ingresar usuario: ")
             contrasena = input("Ingresar contraseña: ")
-            validar_credenciales(usuario, contrasena)
-            while True:
+            verifica_usuario(usuario, contrasena)
+            while True: #Problema
                 operaciones()
                 return False
         elif operacion0 == "2":
             usuario = input("Crear usuario: ")
             contrasena = input("Crear contraseña: ")
-            crear_usuario(usuario, contrasena)
+            crear_usuario(usuario, contrasena, 'usuarios.csv')
         elif operacion0 == "0":
             #Se sale del ciclo y cierra el programa
             continuar0 = "b"
@@ -49,24 +52,40 @@ def operaciones():
             funciones.multiplicar()
         elif operacion== "4":
             #codigo para dividir
-            funciones.dividir()
+            try:
+                funciones.dividir()
+            except ZeroDivisionError:
+                print("No se puede dividir entre 0")
         elif operacion== "0":
             #Finalizar
             continuar = False
         else:
             print('Entrada incorrecta')
 
-def crear_usuario(usuario, contrasena):
-    with open("password.txt", "a") as archivo:
-        archivo.write(f"{usuario},{contrasena}\n")
+def crear_usuario(usuario, contrasena, archivo):
+    with open(archivo, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([usuario, contrasena])
 
-def validar_credenciales(usuario, contrasena):
-    with open("password.txt", "r", encoding="utf-8") as archivo:
-        for linea in archivo:
-            usuario_archivo, contrasena_archivo = linea.strip().split(",")
-            if usuario == usuario_archivo and contrasena == contrasena_archivo:
-                return True
-        return False
+def leer_archivo_csv(archivo:str)->str:
+    '''lee el archivo y regresa un lista de listas'''
+    data = []
+    try:
+        with(open(archivo,'r', encoding='utf-8')) as a:
+            data = csv.reader(a)
+            data = [linea for linea in data]
+    except(FileNotFoundError, IOError):
+        print(f'No existe el archivo {archivo}')
+    return data
+
+def verifica_usuario(usuario:str, contrasena:str)->bool:
+    '''Verifica que el usuario y la contraseña sean correctos'''
+    lista = leer_archivo_csv("usuarios.csv")
+    diccionario = {usuario[0]:usuario[1] for usuario in lista}
+    if usuario in diccionario:
+        if diccionario[usuario] == contrasena:
+            return True
+    return False
 
 if __name__ == "__main__":
     main()
